813e3979a9b2b1c4a19e89101e669fd84ca862d3
Timka <vendetta10090@gmail.com>
2024-04-11 00:14:08 +0500

    lil UI coin page and signing for ios

--- lib/features/crypto_coin/view/crypto_coin_screen.dart
+++ lib/features/crypto_coin/view/crypto_coin_screen.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:second/repositories/crypto_coins/crypto_coins_repository.dart';
 import 'package:second/repositories/crypto_coins/models/crypto_coin.dart';
@@ -19,7 +20,8 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
     super.didChangeDependencies();
 
     final args = ModalRoute.of(context)?.settings.arguments;
-    assert(args != null && args is CryptoCoin, "Provide argument of type CryptoCoin!");
+    assert(args != null && args is CryptoCoin,
+        "Provide argument of type CryptoCoin!");
     _coin = args as CryptoCoin;
     setState(() {});
   }
@@ -37,34 +39,48 @@ class _CryptoCoinScreenState extends State<CryptoCoinScreen> {
 
     final theme = Theme.of(context);
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(_coin!.name, style: const TextStyle(color: Colors.white)),
-          centerTitle: true,
-          leading: IconButton(icon: const Icon(Icons.arrow_back_ios), onPressed: () => Navigator.pop(context), color: Colors.white,),
-          backgroundColor: Colors.black,
-          
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(20),
-          // color: Colors.black,
-          decoration: BoxDecoration( 
-            color: Colors.black.withAlpha(240),
-            // boxShadow: [BoxShadow(color: Colors.black, offset: Offset.fromDirection(1.0))]
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(_coin!.name,
+                  style: const TextStyle(color: Colors.white)),
+              centerTitle: true,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back_ios),
+                onPressed: () => Navigator.pop(context),
+                color: Colors.white,
+              ),
+              backgroundColor: Colors.black,
             ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            body: Container(
+              padding: const EdgeInsets.all(20),
+              // color: Colors.black,
+              decoration: BoxDecoration(
+                color: Colors.black.withAlpha(240),
+                // boxShadow: [BoxShadow(color: Colors.black, offset: Offset.fromDirection(1.0))]
+              ),
+              child: Column(
                 children: [
-                  Text('Current price: ', style: theme.textTheme.titleLarge),
-                  Text('\$${_coin!.priceInUSD.toStringAsFixed(3)}', style: theme.textTheme.titleLarge)
+                  DecoratedBox(
+                    decoration: const BoxDecoration(
+                        color: Color.fromARGB(255, 34, 35, 32),
+                        borderRadius: BorderRadius.all(Radius.circular(12))),
+                    child:
+                        Image.network(_coin!.imageURL, height: 120, width: 120),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Current price: ',
+                          style: theme.textTheme.titleLarge),
+                      Text('\$${_coin!.priceInUSD.toStringAsFixed(3)}',
+                          style: theme.textTheme.titleLarge)
+                    ],
+                  ),
                 ],
               ),
-            ],
-          ),
-        )
-      )
-    );
+            )));
   }
 }
